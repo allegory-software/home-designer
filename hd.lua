46bf01b43bcf06cf93ce73fe900d486676924fc0
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2021-05-22 02:53:30 +0300

    unimportant

--- hd.lua
+++ hd.lua
@@ -1,8 +1,13 @@
 
-if not ... then require'hd_http'; return end
+local ffi = require'ffi'
+ffi.tls_libname = 'tls_bearssl'
+--ffi.tls_libname = 'tls_libressl'
+local server = require'http_server'
+--local libtls = require'libtls'
+--libtls.debug = print
 
 require'$'
-require'webb'
+local respond = require'webb'
 require'webb_action'
 require'webb_query'
 require'webb_spa'
@@ -13,6 +18,20 @@ require'xmodule'
 --require'x_dba'
 
 require'hd_conf'
+
+math.randomseed(require'time'.time())
+
+config('host', 'localhost')
+config('main_module', 'hd')
+config('www_dir', 'hd-www')
+config('var_dir', '.')
+
+config('db_name', 'hd')
+config('db_port', 3307)
+config('db_pass', 'abcd12')
+
+config('session_secret', 'auf9#8xc@kl~24lf_a')
+config('pass_salt', 'ig9sd8l#la;,3xj')
 
 --require'hd_install'
 
@@ -61,7 +80,34 @@ action['404.html'] = function()
 	}
 end
 
-return function()
-	check(action(unpack(args())))
+if ... == 'hd' then --used as module, required by webb's respond() call.
+	return function()
+		check(action(unpack(args())))
+	end
 end
+
+local server = server:new{
+	libs = 'sock sock_libtls zlib',
+	listen = {
+		{
+			--host = 'localhost',
+			tls = true,
+			tls_options = {
+				cert_file = 'localhost.crt',
+				key_file  = 'localhost.key',
+			},
+		},
+		{
+			--host = 'localhost',
+		},
+	},
+	debug = {
+		--protocol = true,
+		--stream = true,
+		tracebacks = true,
+	},
+	respond = respond,
+}
+
+server.start()
 
